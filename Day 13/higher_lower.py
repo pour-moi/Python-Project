4a5613b61fe224ac67cb37d2ab91a7f45e88570a
pour-moi <pour64moi@gmail.com>
2024-06-07 08:25:52 -0400

    result checking funciton refactored

--- Day 13/higher_lower.py
+++ Day 13/higher_lower.py
@@ -8,36 +8,30 @@ def start_game():
     game(a, b)
 
 def check_result(user_choice, a, b):
-    score = 0
     print(logo)
-    if user_choice == 'a':
-        if a["follower_count"] > b["follower_count"]:
-            score += 1
-            print(f"You are right! Your current score: {score}")
-            a = b
-            b = choice(data) 
-            game(a, b)
-        else:
-            print(f"Sorry that's wrong :( Final score: {score}")
-    
-    if user_choice == 'b':
-        if a["follower_count"] > b["follower_count"]:
-            print(f"Sorry that's wrong :( Final score {score}")
-        else:
-            score += 1
-            print(f"You are right! Your current score: {score}")
-            a = b
-            b = choice(data) 
-            game(a, b)
+    if a["follower_count"] > b["follower_count"]:
+        return user_choice == 'a'
+    else:
+        return user_choice == 'b'
 
 print(logo)
 
 def game(a, b):
+    score = 0
     print(f"Compare A: {a["name"]}, a {a["description"]}, from {a["country"]}")
     print(vs)
     print(f"Against B: {b["name"]}, a {b["description"]}, from {b["country"]}")
 
     user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
-    check_result(user_choice, a, b)
+    result = check_result(user_choice, a, b)
+    if result:
+        score += 1
+        print(f"You are right! Your current score: {score}")
+        a = b
+        b = choice(data) 
+        game(a, b)
+    else:
+        print(f"Sorry that's wrong :( Final score {score}")
+
 
 start_game()
